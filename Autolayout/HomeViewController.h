911536c48c38447d4b5760c78c5202e8f0ef3663
Prem Kumar <thatzprem@gmail.com>
2014-02-28 11:14:36 +0530

    Uploading to Github

---
+++ Autolayout/HomeViewController.h
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.h
+//  Autolayout
+//
+//  Created by Prem kumar on 27/02/14.
+//  Copyright (c) 2014 nexTip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+/**
+ Sample test documentation method for understatnding.
+ 
+ @param stringParamOne Sample string param one.
+ @param stringParamTwo Sample string param two.
+ @return NSArray Array of string params object passed to the method.
+ */
+
+-(NSArray *)testDocumentationMethodWithParamOne:(NSString *)stringParamOne andParamTwo:(NSString *)stringParamTwo;
+    
+@end
---
+++ Autolayout/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  Autolayout
+//
+//  Created by Prem kumar on 27/02/14.
+//  Copyright (c) 2014 nexTip. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//  Highlight the method name and look for quick help in attribute inspector tab.
+
+-(NSArray *)testDocumentationMethodWithParamOne:(NSString *)stringParamOne andParamTwo:(NSString *)stringParamTwo{
+    
+    NSArray *testArray;
+        //Sample code
+    return testArray;
+}
+
+@end
